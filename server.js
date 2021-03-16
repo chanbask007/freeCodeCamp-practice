be1ce49d3f67ebd6cdee90a557437f10edc62c89
Chanbas Kalge <chanbask007@gmail.com>
2021-03-16 11:12:45 +0530

    pug views task completed

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+
+// const pug = require('pug')
+
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
-
+app.set('view engine', 'pug')
+app.set('views', process.cwd() + '/views/pug')
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render(process.cwd() + '/views/pug');
 });
 
 const PORT = process.env.PORT || 3000;
